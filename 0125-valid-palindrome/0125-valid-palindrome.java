dbd4596d8a825c4b51307dc35efd5468f8cd8a94
Vaishnavidhomane <vaishnavidhomane29@gmail.com>
2026-07-02 13:40:22 +0530

    Time: 4 ms (49.31%), Space: 44.1 MB (84.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,8 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
-        s=s.replaceAll("[^a-z0-9]", "");
-        String reverse=new StringBuilder(s).reverse().toString();
-        return s.equals(reverse);
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            char left=s.charAt(i);
+             char right=s.charAt(j);
+            if(!Character. isLetterOrDigit(left)){
+                i++;
+                continue;
+
+            }
+            if(!Character. isLetterOrDigit(right)){
+                j--;
+                continue;
+
+            }
+            if(Character.toLowerCase(left)!=Character.toLowerCase(right)){
+                return false;
+
+            }
+        i++;
+        j--;
+
+        }
+        return true;
     }
 }
