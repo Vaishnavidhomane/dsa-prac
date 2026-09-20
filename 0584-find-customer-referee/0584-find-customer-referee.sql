b381666037e2887a555440a337162c6f399ddbed
Vaishnavidhomane <vaishnavidhomane29@gmail.com>
2026-09-20 23:50:49 +0530

    Time: 395 ms (5.42%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
-Select name from Customer
-where referee_id != 2
-or referee_id is null;
+-- Write your PostgreSQL query statement below
+select name from Customer
+where referee_id != 2 or referee_id is null;
