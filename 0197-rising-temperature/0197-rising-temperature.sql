6c3b6363f4456a0a5323fd61f215e55eecf22892
Vaishnavidhomane <vaishnavidhomane29@gmail.com>
2026-09-21 19:34:23 +0530

    Time: 282 ms (30.1%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2
-ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
-WHERE w1.temperature > w2.temperature;
+-- Write your PostgreSQL query statement below
+select w1.id
+from weather w1, weather w2
+where w1.recordDate - w2.recordDate =1
+and w1.temperature> w2.temperature
